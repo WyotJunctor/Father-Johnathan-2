99d8d4ac2c80b1b74e556d6daac307065eaca5aa
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2020-02-03 00:57:26 -0500

    hahahahahaha

---
+++ FatherJohnathan2/Assets/Scripts/Pizza.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Opsive.UltimateCharacterController.Traits;
+
+public class Pizza : MonoBehaviour, IInteractableTarget
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Can the target be interacted with?
+    /// </summary>
+    /// <param name="character">The character that wants to interactact with the target.</param>
+    /// <returns>True if the target can be interacted with.</returns>
+    public bool CanInteract(GameObject character)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Interact with the target.
+    /// </summary>
+    /// <param name="character">The character that wants to interactact with the target.</param>
+    public void Interact(GameObject character)
+    {
+        // play pizza pick up event
+    }
+}
